94a65e80b2d2535364560566a11ba9fe78468fee
stephin.abraham <stephin.abraham@accenture.com>
2026-01-27 12:01:06 +0530

    config updated

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,12 +2,17 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
     testDir: './tests',
+    timeout: 30 * 1000,
+    expect: {
+        timeout: 5 * 1000
+    },
     fullyParallel: false,
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
     workers: 1,
     reporter: [
         ['line'],
+        ['html', { open: 'never' }],
         ['allure-playwright', {
             detail: true,
             outputFolder: 'allure-results',
@@ -15,6 +20,8 @@ export default defineConfig({
         }]
     ],
     use: {
+        actionTimeout: 10 * 1000,
+        navigationTimeout: 15 * 1000,
         trace: 'on-first-retry',
         screenshot: 'only-on-failure',
         video: 'retain-on-failure',
